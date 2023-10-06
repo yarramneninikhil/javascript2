134b70e0dc59999421ca1ea24359f775ef5bad75
Nikhil <yarramneninikhil@gmail.com>
2023-10-06 23:08:16 +0530

    project of Dom

---
+++ index.js
@@ -0,0 +1,5 @@
+const items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "900";
+}
